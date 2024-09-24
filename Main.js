89d1ef5736ba0467d7992d55629c2a8aee49708d
Aqib Iqbal <aqibiqbal.dev@gmail.com>
2024-09-25 00:33:08 +0530

    Update Main.js

--- Main.js
+++ Main.js
@@ -1,11 +1,2 @@
-import React from 'react'
-
-function Main({children}) {
-  return (
-      <div>
-          {children}
-    </div>
-  )
-}
-
-export default Main
+let isValid = true
+export default isValid
